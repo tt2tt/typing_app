203bb8f5602683b38214f797334a6a64cea7a24b
tt <groveleffect@gmail.com>
2025-09-02 21:02:58 +0900

    トップページの作成

---
+++ frontend/src/app/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="w-full py-6 bg-blue-600 text-white text-center shadow-md">
+      <h1 className="text-2xl font-bold">タイピングアプリ</h1>
+    </header>
+  );
+};
+
+export default Header;
